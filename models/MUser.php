c3a2827201dd2511a88b62de4da600e805a3542b
rouaix <daniel@rouaix.com>
2018-07-25 15:53:16 +0200

    Ajout de méthodes

--- models/MUser.php
+++ models/MUser.php
@@ -29,7 +29,7 @@ class MUser extends Model
         }
     }
 
-    /** Ajout d'un nouveau user */
+    /* Ajout d'un nouveau user */
     public function insertUser($user_nom,$user_prenom,$user_email,$user_motdepasse){
         $db = parent::connect();
         $ql = ' INSERT
@@ -45,7 +45,6 @@ class MUser extends Model
         ));
     }
 
-    /** Efface user */
     public function deleteUser($id){
         $db = parent::connect();
         $ql = ' DELETE * FROM users WHERE user_id = '.$id.'';
@@ -53,7 +52,26 @@ class MUser extends Model
         $query->execute();
     }
 
-    public function updateUser($id,$champ,$valeur){
+    public function updateUser($tableau){ /* Utilise un tableau type champ= valeur */
+        $db = parent::connect();
+        $ql = 'UPDATE user set (';
+        foreach($tableau as $key => $value){
+            if ($key != 'user_id') {
+                if($value !=""){
+                    if($key =="user_motdepasse"){
+                        $value = password_hash($value, PASSWORD_DEFAULT);
+                    }
+                    $sql .= $key . '=\'' . $value . '\',';
+                }
+            }
+            $sql = rtrim($sql, ",");
+        }
+        $sql .= ') WHERE user_id = '.$tableau["user_id"].'';
+        $query = $db->prepare($sql);
+        $query->execute();
+    }
+
+    public function updateChampUser($id,$champ,$valeur){ /* MAJ d'un champ unique */
         $db = parent::connect();
         $ql = ' UPDATE user set ('.$champ.' = "'.$valeur.'") WHERE user_id = '.$id.'';
         $query = $db->prepare($sql);
@@ -67,6 +85,20 @@ class MUser extends Model
         $query->execute();
         $infoUser = $query->fetchAll();
         return $infoUser;
+    }
+
+    public function verificationMotDePasseUser($email, $motdepasse){
+        $db = parent::connect();
+        $sql = 'select * from users where user_id = '".$id."' limit 1';
+        $query = $db->prepare($sql);
+        $query->execute();
+        $infoUser = $query->fetchAll();
+
+        if (password_verify ($motdepasse , $infoUser["user_motdepasse"])){
+            return true;
+        }else{
+            return false;
+        }
     }
 
     public function getAll() {
